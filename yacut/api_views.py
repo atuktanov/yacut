f5606d9a050e6f9dce21c35ae796654eaf3111be
atuktanov <atuktanov@gmail.com>
2022-08-23 12:05:51 +0300

    Готово для ревью 2

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from re import match
 
 from flask import jsonify, request
@@ -21,19 +22,21 @@ def create_id():
     if not data.get('custom_id'):
         data['custom_id'] = get_unique_short_id()
     if not match(r'^[A-Za-z0-9]{1,16}$', data['custom_id']):
-        raise InvalidAPIUsageError('Указано недопустимое имя для короткой ссылки')
+        raise InvalidAPIUsageError(
+            'Указано недопустимое имя для короткой ссылки')
     if URL_map.query.filter_by(short=data['custom_id']).first():
         raise InvalidAPIUsageError(f'Имя "{data["custom_id"]}" уже занято.')
     url_map = URL_map()
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify(url_map.to_dict()), 201
+    return jsonify(url_map.to_dict()), HTTPStatus.CREATED
 
 
 @app.route('/api/id/<string:short_id>/')
 def get_url(short_id):
     url_map = URL_map.query.filter_by(short=short_id).first()
     if not url_map:
-        raise InvalidAPIUsageError('Указанный id не найден', 404)
+        raise InvalidAPIUsageError(
+            'Указанный id не найден', HTTPStatus.NOT_FOUND)
     return jsonify({'url': url_map.original})
--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from flask import jsonify, render_template
 
 from . import app, db
@@ -5,7 +7,7 @@ from . import app, db
 
 class InvalidAPIUsageError(Exception):
 
-    def __init__(self, message, status_code=400):
+    def __init__(self, message, status_code=HTTPStatus.BAD_REQUEST):
         super().__init__()
         self.message = message
         self.status_code = status_code
@@ -16,13 +18,13 @@ class InvalidAPIUsageError(Exception):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @app.errorhandler(InvalidAPIUsageError)
